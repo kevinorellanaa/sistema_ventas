61a19420fbdfb861d903cd8ae8de78cd280892ba
kevinorellanaa <orellanakevin.a@gmail.com>
2020-04-04 14:34:53 -0600

    cambios nuevos

--- sistema_ventas/settings.py
+++ sistema_ventas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g2qp(wr))#u!@s9r7)u6-vkdksvf06&gux0b^aepg9tg8ryk8+'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
